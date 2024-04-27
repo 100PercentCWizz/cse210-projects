570fa9f9ab512628608fa09a7b80c7bc92ba3fb4
Christian Haroldsen <ctharoldsen@gmail.com>
2024-04-27 10:06:56 -0600

    Completed Prep1 and Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage as a number: ");
+        string strGrade = Console.ReadLine();
+        int intGrade = int.Parse(strGrade);
+        string letterGrade;
+
+        if (intGrade >= 90) {
+            letterGrade = "A";
+        } else if (intGrade >= 80) {
+            letterGrade = "B";
+        } else if (intGrade >= 70) {
+            letterGrade = "C";
+        } else if (intGrade >= 60) {
+            letterGrade = "D";
+        } else {
+            letterGrade = "F";
+        }
+
+        if (intGrade % 10 >= 7 && (letterGrade != "A" && letterGrade != "F")) {
+            letterGrade = letterGrade + "+";
+        } else if (intGrade % 10 < 3 && letterGrade != "F") {
+            letterGrade = letterGrade + "-";
+        }
+
+        Console.WriteLine($"Your grade: {letterGrade}");
+
+        if (intGrade >= 70) {
+            Console.Write("Congrats!  You passed the class!");
+        } else {
+            Console.Write("You did not pass the class but you can certainly try again!");
+        }
     }
 }
