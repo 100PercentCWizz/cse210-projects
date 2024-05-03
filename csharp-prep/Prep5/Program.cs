aef2947ca341d75ed8b5a801050d2b5cab4d567c
Christian Haroldsen <ctharoldsen@gmail.com>
2024-05-03 13:46:08 -0600

    Completed prep5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName() {
+        Console.WriteLine("Please enter your name: ");
+        string str = Console.ReadLine();
+        return str;
+    }
+
+    static int PromptUserNumber() {
+        Console.WriteLine("Please enter your favorite number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        return num;
+    }
+
+    static int SquareNumber(int integer) {
+        return integer * integer;
+    }
+
+    static void DisplayResult(string name, int integer) {
+        Console.WriteLine($"{name}, the square of your number is {integer}.");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int SqrNum = SquareNumber(num);
+        DisplayResult(name, SqrNum);
     }
 }
